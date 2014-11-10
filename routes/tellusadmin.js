19bda3f7e651919d4b05e145bd2c93358a4f5141
Shingo <shingo@gmail.com>
2014-11-10 05:24:22 -0500

    add godmode godmode is for the first time running this app, if you are in the god mode, you would be able to create a admin user with telluadmin routing.
    use "npm godmode" to enter the godmode

--- routes/tellusadmin.js
+++ routes/tellusadmin.js
@@ -2,7 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.render('tellusadmin'); 
+  if(process.argv[2] === 'godmode'){
+    res.render('tellusadmin', {
+      title: '管理區 - for god',
+      godmode: true
+    });
+  } else {
+    res.render('tellusadmin', {
+      title: '管理區',
+      godmode: false
+    });        
+  }
 });
 
 module.exports = router;
