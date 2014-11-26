64180aede9a5d71d8852fb0223185eca40123883
shingo0620 <shingo@gmail.com>
2014-11-25 21:28:52 -0500

    Typo correction

---
+++ config/config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+  cookieSecret: 'catinthecookie',
+  apiKey: '5566isTheBest',
+  db: {
+    production: 'mongodb://localhost:27017/LOLTube',
+    develop: 'mongodb://localhost:27017/test'
+  }
+};
